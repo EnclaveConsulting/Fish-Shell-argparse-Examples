7334fdd2096d26fd159719dfd16b189b457f12d5
Jason Fossen <JasonFossen@users.noreply.github.com>
2025-04-19 20:56:57 -0500

    Update 230_exclusive-options.fish

--- 230_exclusive-options.fish
+++ 230_exclusive-options.fish
@@ -16,3 +16,7 @@ foofunc --orange=4 --down=5
 # Forbidden
 foofunc --apple=2 --orange=4
 foofunc --up=3 --down=5
+
+# --exclusive takes a comma-delimited list of options
+# that cannot be used together.  Multiple uses of
+# --exclusive is permitted, as shown in this example.
