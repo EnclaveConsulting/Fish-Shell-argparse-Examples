58368cfb8aed9a2497555225952cacb6a23efa6f
Jason Fossen <JasonFossen@users.noreply.github.com>
2025-04-19 20:40:23 -0500

    Update 100_check-if-set.fish

--- 100_check-if-set.fish
+++ 100_check-if-set.fish
@@ -20,3 +20,14 @@ foofunc --year=1979
 foofunc --help
 foofunc --year=1979 --help
 foofunc
+
+# When there is an option that does not require an argument (y/year=?)
+# and that option is not used when calling a function, the flag variable
+# that is normally created to hold the argument to that option is 
+# not created at all.  Test for a variable with "set --query".
+
+# When an option that never takes an argument (h/help) is
+# not used when calling the function, the flag variable for 
+# that option ($_flag_h) is not created at all.  If that variable
+# is created, it does not contain 0, true or $true (it is not 
+# actually a boolean), it contains other text.
