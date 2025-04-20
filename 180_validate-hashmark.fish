56fcb645362bbd6f420e436eaf3ab7c24c0acd6b
Jason Fossen <JasonFossen@users.noreply.github.com>
2025-04-19 20:49:09 -0500

    Update 180_validate-hashmark.fish

--- 180_validate-hashmark.fish
+++ 180_validate-hashmark.fish
@@ -12,15 +12,20 @@ foofunc -y -1979
 foofunc -1979
 foofunc --1979
 
-# Flag must be a positive or negative whole integer
+# Note the "#" in the "y#year" option string, not "/".
+# Argument must be a positive or negative whole integer
 # and match the "^--?\d+$" regex pattern, hence,
-# the following examples fail:
+# the following examples will fail:
+
 foofunc --year apple
 foofunc -y 0xFF
 foofunc -+33
 foofunc -3.141592
 
+# Even the short option name can be omitted (-1979).
+# Negative arguments are allowed (--1979).
+
 # The "argparse 'y#year'" flag cannot be postfixed with 
-# with "=" or "=?", hence, these examples both fail:
+# with "=" or "=?", hence, these two examples both fail:
 #       argparse 'y#year=' -- $argv ; or return
 #       argparse 'y#year=?' -- $argv ; or return
