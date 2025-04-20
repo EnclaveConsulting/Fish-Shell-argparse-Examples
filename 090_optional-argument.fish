82904f81dac779b4dd3a8d8d12c524a5fdbda75f
Jason Fossen <JasonFossen@users.noreply.github.com>
2025-04-19 20:29:13 -0500

    Update 090_optional-argument.fish

--- 090_optional-argument.fish
+++ 090_optional-argument.fish
@@ -13,8 +13,16 @@ foofunc --xray=33 --year=1979
 foofunc -x33
 foofunc --year=1979
 
-# The following does not fail, but it
-# includes the "=" in the argument, which
-# is most likely not intended or desired:
+# The "=?" postfix in "x/xray=?" indicates that an argument is not required.
+# The first "x" in "x/xray=?" is the short name of the option (-x).
+# The "xray" in "x/xray=?" is the long name of the option (--xray).
+# Use a single dash (-x33) when an argument is not required.
+# Do not use = with a short name when an argument is not required.
+# Long option names are mentioned with two dashes (--xray=1979).
+# Use = with long names when an argument is not required.
+
+# The following does not fail or throw an error, but it
+# includes the "=" in the argument values, which is
+# most likely not intended or desired:
 
 foofunc -x=33 -y=1979
